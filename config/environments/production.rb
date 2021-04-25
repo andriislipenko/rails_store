c2d762b5b5c0bc195ff406ff4e59447f5516fd92
andriislipenko <andrii.slipenko@gmail.com>
2021-04-25 15:17:28 +0300

    mailer host prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'rails-store-test.herokuapp.com' }
 end
